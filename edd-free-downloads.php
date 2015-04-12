d5d3406e4d450e370a144fcce88eb51bb3133f89
Ghost1227 <dgriffiths@section214.com>
2015-04-12 07:41:33 -0500

    Fix passing download_id on mobile devices

--- edd-free-downloads.php
+++ edd-free-downloads.php
@@ -361,7 +361,7 @@ if( ! class_exists( 'EDD_Free_Downloads' ) ) {
             $modal .= '</div>';
 
             $modal .= '<input type="hidden" name="edd_action" value="free_download_process" />';
-            $modal .= '<input type="hidden" name="edd_free_download_id" value="' . $wp_query->query_vars['edd-free-download'] . '" />';
+            $modal .= '<input type="hidden" name="edd_free_download_id" value="' . $wp_query->query_vars['download_id'] . '" />';
             $modal .= '<input type="button" name="edd_free_download_submit" class="edd-free-download-submit edd-submit button ' . $color . '" value="' . $label . '" />';
             $modal .= '<input type="button" name="edd_free_download_cancel" class="edd-free-download-cancel edd-submit button ' . $color . '" value="' . __( 'Cancel', 'edd-free-downloads' ) . '" />';
 
