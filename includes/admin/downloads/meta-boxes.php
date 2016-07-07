d9863b7c305577e337c17c58fa5daeac0de64fce
fxbenard <fxbenard@gmail.com>
2016-07-07 16:18:18 +0200

    WPCS + Wrong textdomain

--- includes/admin/downloads/meta-boxes.php
+++ includes/admin/downloads/meta-boxes.php
@@ -6,9 +6,8 @@
  * @since       1.2.0
  */
 
-
-// Exit if accessed directly
-if( ! defined( 'ABSPATH' ) ) {
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
@@ -17,24 +16,24 @@ if( ! defined( 'ABSPATH' ) ) {
  * Bypass Free Downloads settings
  *
  * @since       1.2.0
- * @param       int $post_id Download (Post) ID
+ * @param       int $post_id Download (Post) ID.
  * @return      void
  */
 function edd_free_downloads_render_bypass_options( $post_id = 0 ) {
-	if( ! current_user_can( 'manage_shop_settings' ) ) {
+	if ( ! current_user_can( 'manage_shop_settings' ) ) {
 		return;
 	}
 
 	$bypass = get_post_meta( $post_id, '_edd_free_downloads_bypass', true ) ? 1 : 0;
 	?>
 	<p><strong><?php _e( 'Bypass Free Downloads Modal:', 'edd-free-downloads' ); ?></strong></p>
-	<label for="_edd_free_downloads_bypass">
+    <label for="_edd_free_downloads_bypass">
 		<?php echo EDD()->html->checkbox( array(
 			'name'    => '_edd_free_downloads_bypass',
-			'current' => $bypass
+			'current' => $bypass,
 		) ); ?>
-		<?php _e( 'Bypass the Free Downloads modal for this download', 'easy-digital-downloads' ); ?>
-	</label>
+		<?php _e( 'Bypass the Free Downloads modal for this download', 'edd-free-downloads' ); ?>
+    </label>
 <?php
 }
 add_action( 'edd_meta_box_settings_fields', 'edd_free_downloads_render_bypass_options', 30 );
@@ -44,7 +43,7 @@ add_action( 'edd_meta_box_settings_fields', 'edd_free_downloads_render_bypass_op
  * Add our meta box fields to the save array
  *
  * @since       1.2.0
- * @param       array $fields The existing fields array
+ * @param       array $fields The existing fields array.
  * @return      array The updated fields array
  */
 function edd_free_downloads_meta_box_save( $fields ) {
