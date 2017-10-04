25ccf06bbabe0910786584778ea2483d399beff4
Dan Beil <dan@webdevstudios.com>
2017-10-04 13:13:24 -0500

    commenting out dev code

--- includes/filters.php
+++ includes/filters.php
@@ -126,11 +126,13 @@ function edd_free_downloads_purchase_download_form( $purchase_form, $args ) {
 							 * to allow for end user to manipulate data as needed
 							 * per https://github.com/easydigitaldownloads/edd-free-downloads/issues/152
 							 */
-							$download_post = new EDD_Download( $download_id );
-							$download_post = json_encode( $download_post );
+							// $download_post = new EDD_Download( $download_id );
+							// $download_post = json_encode( $download_post );
 
 							echo apply_filters( 'edd_free_downloads_button_override', sprintf(
-								'<a class="edd-add-to-cart %1$s" href="' . $href . '" data-download-id="%3$s" data-download-post="' . $download_post . '">%2$s</a>',
+								'<a class="edd-add-to-cart %1$s" href="' . $href . '" data-download-id="%3$s">%2$s</a>',
+
+								// '<a class="edd-add-to-cart %1$s" href="' . $href . '" data-download-id="%3$s" data-download-post="' . $download_post . '">%2$s</a>',
 								$download_class,
 								$download_label,
 								$download_id
